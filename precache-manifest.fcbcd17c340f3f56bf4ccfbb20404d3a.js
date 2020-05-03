dc5cf7a03e7c82829db25c8a7e7432ff2b854743
Shuhei Aoyama <mt.blue81@gmail.com>
2020-05-04 08:59:04 +0900

    Updates

---
+++ precache-manifest.fcbcd17c340f3f56bf4ccfbb20404d3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634f8d10036291574e5870f471550055",
+    "url": "/react-boilerplate/index.html"
+  },
+  {
+    "revision": "fe7e7fa1789ff60cabef",
+    "url": "/react-boilerplate/static/css/main.722be02a.chunk.css"
+  },
+  {
+    "revision": "73979103ce2efd032c67",
+    "url": "/react-boilerplate/static/js/2.a393140f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-boilerplate/static/js/2.a393140f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe7e7fa1789ff60cabef",
+    "url": "/react-boilerplate/static/js/main.9e73c97c.chunk.js"
+  },
+  {
+    "revision": "0d6b96f9e2bab538abba",
+    "url": "/react-boilerplate/static/js/runtime-main.aa416d71.js"
+  }
+]);
